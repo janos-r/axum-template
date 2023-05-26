f796bd998f181e02d5aebafdb78f65310fb07835
Radim <radim.janos@seznam.cz>
2023-05-27 02:41:50 +0800

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
+use std::net::SocketAddr;
+
+use axum::routing::get;
+use axum::{response::Html, Router};
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let routes_hello = Router::new().route(
+        "/hello",
+        get(|| async { Html("Hello <strong>World!!!</strong>") }),
+    );
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("->> LISTENING on {addr}");
+    axum::Server::bind(&addr)
+        .serve(routes_hello.into_make_service())
+        .await
+        .unwrap();
 }
