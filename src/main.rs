84f410b33f9f73fc71a433872e862a69c517955f
Radim <radim.janos@seznam.cz>
2023-05-27 22:14:27 +0800

    route static fs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use axum::extract::{Path, Query};
 use axum::response::{Html, IntoResponse};
-use axum::routing::get;
+use axum::routing::{get, get_service};
 use axum::Router;
 use serde::Deserialize;
 use std::net::SocketAddr;
+use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
-    let routes_all = Router::new().merge(routes_hello());
+    let routes_all = Router::new()
+        .merge(routes_hello())
+        .fallback_service(routes_static());
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("->> LISTENING on {addr}");
     axum::Server::bind(&addr)
@@ -36,5 +39,10 @@ async fn main() {
         Router::new()
             .route("/hello", get(handle_hello))
             .route("/hello2/:x", get(handle_hello2))
+    }
+
+    // fallback fs
+    fn routes_static() -> Router {
+        Router::new().nest_service("/", get_service(ServeDir::new("./")))
     }
 }
